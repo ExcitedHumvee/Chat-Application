44a56a9a50de409340c282bae680172cef6a7fe1
ExcitedHumvee <35200248+ExcitedHumvee@users.noreply.github.com>
2023-10-08 13:20:13 +0300

    added ChatController, ChatMessage and MessageType

---
+++ src/main/java/com/stanydesa/chat/chat/ChatController.java
@@ -0,0 +1,25 @@
+package com.stanydesa.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")//url to invoke
+    @SendTo("/topic/public")//sent to the queue
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {//normally we send RequestBody, but here payload
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
---
+++ src/main/java/com/stanydesa/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.stanydesa.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
